093e23824db045e6af0c8a7fede9aa19c2927838
ayushnagar123 <an431999@gmail.com>
2020-04-04 02:18:40 +0530

    heroku hosting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 // import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Component/Home'
+import About from './Component/About'
+import Login from './Component/Login.js'
 import './App.css';
 import Header from './Component/Header'
 function App() {
   return (
     <div className="App">
-        <Header/>
+      <Header/>
+      <div className="main">
+        <Router>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
