3251c7b234d6bac7a8420e291262b7f707613845
ayushnagar123 <an431999@gmail.com>
2020-04-06 10:00:38 +0530

    bot installation

--- src/App.js
+++ src/App.js
@@ -10,18 +10,20 @@ import {
 // import Header from './Component/Header'
 import About from './Component/About'
 import Home from './Component/Home'
+import Register from './Component/Register'
+
 import Cookies from 'js-cookie';
 import { Button,Navbar,Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Component/Header.css'
-var clientID,frontend;
+var clientID;
+var frontend = window.location.origin;
+console.log(frontend);
 if(process.env.NODE_ENV==="production"){
-  frontend = "https://codebadge-frontend.netlify.com/"
   clientID="9a3d93461f11673e4164"
 }
 else{
-frontend = "http://localhost:3000"
-clientID="90483cf9b8d2f46424fa"
+  clientID="90483cf9b8d2f46424fa"
 }
 function login(){
     window.location.href=`https://github.com/login/oauth/authorize?client_id=${clientID}`;
@@ -38,13 +40,13 @@ function App() {
     <div className="App">
       <Router>
       <Navbar className="header" bg="white" expand="lg">
-            <Navbar.Brand class="toolbar"><Link to="/" className="light"><h2>Codebadge</h2></Link></Navbar.Brand>
+            <Navbar.Brand className="toolbar"><Link to="/" className="light"><h2>Codebadge</h2></Link></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav" className="justify-content:end">
                 <Nav className="ml-auto toolbar-button" >
                     <Nav.Link><Link to="/about" className="light"><Button variant="light">About Us</Button></Link></Nav.Link>
                     <Nav.Link><Link to="/" className="light"><Button variant="light" className="light">Join Us</Button></Link></Nav.Link>
-                    <Nav.Link><Link to="/" className="primary"><Button variant="primary" onClick={login} className="primary">Login <i class="fa fa-github" aria-hidden="true"></i></Button></Link></Nav.Link>
+                    <Nav.Link><Link to="/" className="primary"><Button variant="primary" onClick={login} className="primary">Login <i className="fa fa-github" aria-hidden="true"></i></Button></Link></Nav.Link>
                  </Nav>
             </Navbar.Collapse>
         </Navbar>
@@ -79,6 +81,9 @@ function App() {
           </Navbar>
           <div className="main mt-15">
           <Switch>
+          <Route path="/register">
+              <Register/> 
+            </Route>
             <Route path="/about">
               <About/> 
             </Route>
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,11 +1,24 @@
 import React from 'react';
-
+import Cookies from 'js-cookie'
 const Home = () => {
+    if(!Cookies.get("token"))
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <div>
+                <h1>Codebadge Project</h1>
+                <p>The project which stands for the rights of the developers.</p>
+            </div>
+        </>
     );
+    else{
+        return(
+            <>
+                <div>
+                    
+                </div>
+            </>
+        )
+    }
 }
 
 export default Home;
